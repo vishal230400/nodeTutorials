cf10b37e966f165b94a755496af6f4d8e868b219
Vishal S <vishalram230400@gmail.com>
2022-05-31 21:29:36 +0530

    fsSync

---
+++ 10-fsModuleSync.js
@@ -0,0 +1,14 @@
+// Reference Url=https://nodejs.org/dist/latest-v18.x/docs/api/fs.html
+
+const { readFileSync, writeFileSync, read } = require("fs");
+
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+console.log(first, second);
+
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result : ${first}, ${second}`,
+  { flag: "a" } //flag a is append without flag argument it will clear the file and rewrite
+);
