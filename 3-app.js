bc977ddddfa8ebf1bd4a272c082ab84bc0122b34
Vishal S <vishalram230400@gmail.com>
2022-05-31 17:32:18 +0530

    changes

--- 3-app.js
+++ 3-app.js
@@ -3,6 +3,8 @@ const { john, peter } = require("./4-names");
 const sayHi = require("./5-utils");
 const data = require("./6-alternativeSyntax");
 
+require("./7-mindGrenade");
+
 console.log(data);
 console.log(names);
 console.log(sayHi);
---
+++ 6-alternativeSyntax.js
@@ -0,0 +1,1 @@
+module.exports.items = ["item1", "item2"];
