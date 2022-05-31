4cd0649ba7cc5b005a5d372b59e13d393dee99d8
Vishal S <vishalram230400@gmail.com>
2022-05-31 16:36:06 +0530

    init

---
+++ numberGreatLessEqualTo10.js
@@ -0,0 +1,13 @@
+const amount = 12;
+
+console.log(
+  "Program to find whether the Number Greater/Lesser/Equal to 10 !!!"
+);
+
+if (amount < 10) {
+  console.log(`Given number ${amount} is less than 10`);
+} else if (amount == 10) {
+  console.log(`Given number ${amount} is equal to 10`);
+} else {
+  console.log(`Given number ${amount} is greater than 10`);
+}
