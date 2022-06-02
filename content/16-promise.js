f82b503c2fccc6f446ee672f904058b9c7640cd5
Vishal S <vishalram230400@gmail.com>
2022-06-02 14:25:22 +0530

    promise

---
+++ content/16-promise.js
@@ -0,0 +1,38 @@
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    console.log(first, second);
+    await writeFilePromise(
+      "./content/resultPromise",
+      `Promise result: ${first} , ${second}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+/*
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./content/first.txt")
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+*/
